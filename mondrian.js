e5ee3666e0ba583dd1fee9ffea7763c488768c64
yuxuanliu888 <55365266+yuxuanliu888@users.noreply.github.com>
2019-09-21 21:24:52 -0700

    Create mondrian.js

---
+++ mondrian.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  stroke(0, 333, 388)
+  line(1000, 0, 0, 100)
+  strokeWeight(30)
+  strokeWeight(3)
+  fill(0, 0, 90)
+  stroke(39)
+  triangle(9, 8, 333, 2, 399, 888)
+  line(10, 0, 0, 100)
+  fill(30, 40, 300)
+  triangle(1000, 0, 0, 100, 10, 0)
+  stroke(588, 033, 299)
+  fill(299, 577, 9)
+  triangle(199, 0, 0, 3, 0, 300)
+  stroke(9)
+  fill(0)
+  strokeWeight(3)
+  triangle(3, 7, 29, 0, 3, 82)
+
+
+}
